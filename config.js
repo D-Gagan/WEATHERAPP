f01b7cd6fcc8bcb7a221592122cad92b6d9d5843
Gagan D <76513704+GaganD777@users.noreply.github.com>
2023-01-21 01:29:38 +0530

    Add

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"41e6997817434777f7d25313f93f713c"
+    }
+}
+module.exports=constants;
